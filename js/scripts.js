49981f3d63d8ac52d83f1edcc64678f4fc2edce6
Caleb Paul and Karen Freeman-Smith <caleb@>
2016-08-17 11:40:44 -0700

    moves u with q when the two are together

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 function translator(phrase) {
   for(var i=0; i<phrase.length; i++) {
     if (!"AEIOUaeiou".includes(phrase[0])) {
+      if (((phrase.substr(0,1)==="q")||("Q"===phrase.substr(0,1)))&&
+          ((phrase.substr(1,1)==="u")||("U"===phrase.substr(1,1)))) {
+          phrase = phrase.substr(2) + phrase.substr(0,2);
+      } else {
+        phrase = phrase.substr(1) + phrase.substr(0,1);
+      }
       phrase = phrase.substr(1) + phrase.substr(0,1);
     } else {
       phrase+="ay";
