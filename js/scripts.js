7d01e84ed5a34082c0c596a4f69eced8d0298ce9
Caleb Paul and Karen Freeman-Smith <karen@>
2016-08-17 14:06:51 -0700

    fix bug regarding two letter words and five letter words in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
+
 function translator(phrase) {
-  for(var i=0; i<phrase.length; i++) {
-    if (!"AEIOUaeiou".includes(phrase[0])) {
-      if (((phrase.substr(0,1)==="q")||("Q"===phrase.substr(0,1)))&&
-          ((phrase.substr(1,1)==="u")||("U"===phrase.substr(1,1)))) {
-          phrase = phrase.substr(2) + phrase.substr(0,2);
+  var phraseArray = phrase.split(" ");
+  var newArray=phraseArray.map(translate);
+  phrase = newArray.join(" ");
+  return phrase;
+}
+
+function translate(word) {
+  for(var i=0; i<word.length; i++) {
+    if (!"AEIOUaeiou".includes(word[0])) {
+      if (((word.substr(0,1)==="q")||("Q"===word.substr(0,1)))&&
+          ((word.substr(1,1)==="u")||("U"===word.substr(1,1)))) {
+          word = word.substr(2) + word.substr(0,2);
       } else {
-        phrase = phrase.substr(1) + phrase.substr(0,1);
+        word = word.substr(1) + word.substr(0,1);
       }
-      phrase = phrase.substr(1) + phrase.substr(0,1);
     } else {
-      phrase+="ay";
-      return phrase;
+      word+="ay";
+      return word;
     }
   }
 }
@@ -19,7 +26,6 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     $("#results").text(translator($("#phrase").val()));
-
 
   });
 });
