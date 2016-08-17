e1a24a163a2ffe795a8d656501c5e81c6c153495
Caleb Paul and Karen Freeman-Smith <karen@>
2016-08-17 11:11:12 -0700

    add ay to end of words that start with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
+function translator(phrase) {
+  if("AEIOUaeiou".includes(phrase[0])){
+    phrase+="ay";
+  }
+  return phrase;
+}
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    $("#results").text($("#phrase").val());
+    $("#results").text(translator($("#phrase").val()));
+
+
   });
 });
